edd4fb753bac6c52e6c0c8643acc443a7b56ad86
veda <vedaa8421@gmail.com>
2024-03-11 01:27:07 +0530

    Exam
    Website

---
+++ WordPress/wordpress/wp-content/themes/index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+
+<main>
+    <!-- Your homepage content goes here -->
+    <h2>Welcome to My Website</h2>
+    <p>This is the homepage content.</p>
+</main>
+
+<?php get_footer(); ?>
